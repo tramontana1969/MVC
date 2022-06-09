0197265304b2167666721035a14037f664653c1f
User <snegovskojm@gmail.com>
2022-06-09 13:52:18 +0300

    create Connection class with Configs

---
+++ connection/Connection.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__ . '/../configuration/Config.php';
+
+class Connection
+{
+    public static array $user_data;
+
+    static function connectToRest(string $url, string $method)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, Config::getConfig());
+
+        switch ($method) {
+            case 'get':
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                break;
+            case 'post':
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(self::$user_data, JSON_UNESCAPED_UNICODE));
+                break;
+            case 'put':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(self::$user_data, JSON_UNESCAPED_UNICODE));
+                break;
+            case 'delete':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+                break;
+        }
+        $output = curl_exec($ch);
+        curl_close($ch);
+
+        return $output;
+    }
+}
