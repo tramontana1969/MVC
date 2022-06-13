f6da906a55d48beb73613762ef6f5140be7e9420
User <snegovskojm@gmail.com>
2022-06-13 18:40:28 +0300

    add readability in arrays

--- controller/Controller.php
+++ controller/Controller.php
@@ -22,14 +22,24 @@ class Controller
     public function add($name, $email, $gender, $status)
     {
         if (!empty($name) && !empty($email) && !empty($gender) && !empty($status)) {
-            $this->user->createUser(['name' => $name, 'email' => $email, 'gender' => $gender, 'status' => $status]);
+            $this->user->createUser([
+                'name' => $name,
+                'email' => $email,
+                'gender' => $gender,
+                'status' => $status,
+            ]);
         }
     }
 
     public function edit($id, $name, $email, $gender, $status)
     {
         if (!empty($id) && !empty($name) && !empty($email) && !empty($gender) && !empty($status)) {
-            $this->user->updateUser($id, ['name' => $name, 'email' => $email, 'gender' => $gender, 'status' => $status]);
+            $this->user->updateUser($id, [
+                'name' => $name,
+                'email' => $email,
+                'gender' => $gender,
+                'status' => $status,
+            ]);
         }
 
         header('Location: ../index.php');
