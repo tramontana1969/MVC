7ec5dd0c5194ae5ac9fb4c2dfb1b3124fb03ef76
User <snegovskojm@gmail.com>
2022-06-13 18:15:49 +0300

    delete dispaying errors

--- view/users.php
+++ view/users.php
@@ -1,9 +1,3 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
-
 <html>
 <DOCTYPE html>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
