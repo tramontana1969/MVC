fbdedf0a009d3663cdbe77c501453a474106ca44
User <snegovskojm@gmail.com>
2022-06-13 18:53:12 +0300

    replaced 'html' and 'DOCTYPE' tags

--- view/users.php
+++ view/users.php
@@ -1,5 +1,5 @@
+<!DOCTYPE html>
 <html>
-<DOCTYPE html>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
